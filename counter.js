676cb6024ea62b35c7b4bace4727cac06351d9e5
“Ryan” <pagsmanagement@gmail.com>
2022-04-06 18:30:29 -0600

    push

---
+++ counter.js
@@ -0,0 +1,41 @@
+
+let count = 0;
+let countEl = document.getElementById('count-el')
+let saveEl = document.getElementById('save-el')
+let resetEl = document.getElementById('reset-el')
+let display = document.getElementsByClassName('display')
+
+function increment() {
+    count += 1;
+    countEl.textContent = count;
+    console.log(count)
+}
+function decrement() {
+    if (count > 0)
+    count -= 1;
+    countEl.textContent = count;
+    console.log(count)
+}
+function save() {
+    let message1 = "Woah too much!"
+    if (count >= 7)
+        alert(message1)
+
+    saveEl.textContent = count;
+}
+function reset() {
+    saveEl.textContent = 0;
+    countEl.textContent = 0;
+    count = 0;
+}
+
+// let welcomeEl = document.getElementById('welcome-el')
+// let greeting = "Hi my name is ";
+// let name = "Ryan"
+
+// welcomeEl.innerText = greeting + name;
+
+// welcomeEl.innerText += " 👋"
+
+// console.log(welcomeEl.innerText)
+
